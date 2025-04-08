6698fd00c343c741e52227b866d86c2d51355559
vga <gaganov@octonus.com>
2025-04-08 17:37:56 +0300

    delete line for micro & default script for ubuntu

---
+++ apply-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+SETTINGS=/home/user/settings
+
+VSCODIUM=/home/user/.config/VSCodium
+cp "$SETTINGS/vscodium/keybindings.json" "$VSCODIUM/User/keybindings.json" || exit 1
+cp "$SETTINGS/vscodium/settings.json" "$VSCODIUM/User/settings.json" || exit 1
+
+MICRO=/home/user/.config/micro
+cp "$SETTINGS/micro/bindings.json" "$MICRO/bindings.json" || exit 1
+cp "$SETTINGS/micro/settings.json" "$MICRO/settings.json" || exit 1
+
+GEANY=/home/user/.config/geany
+cp "$SETTINGS/geany/geany.conf" "$GEANY/geany.conf" || exit 1
+cp "$SETTINGS/geany/keybindings.conf" "$GEANY/keybindings.conf" || exit 1
+
+DOUBLECMD=/home/user/.config/doublecmd
+cp "$SETTINGS/doublecmd/doublecmd.xml" "$DOUBLECMD/doublecmd.xml" || exit 1
+cp "$SETTINGS/doublecmd/shortcuts.scf" "$DOUBLECMD/shortcuts.scf" || exit 1
+
+echo "Settings applied successfully!"
